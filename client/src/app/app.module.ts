b1037091649ce5a5fc0a3edf879813166c7a64f6
Adrian Fernandez <adrianfernandez@paradigmadigital.com>
2017-01-30 11:23:24 +0100

    new dependencies y postman

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,19 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+//Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+//AF2 Settings 
+export const firebaseConfig = {
+    apiKey: "AIzaSyBfr-JM_Al_eqoa0B4NavPHy8wYN1k8fDU",
+    authDomain: "quidoo-87f0a.firebaseapp.com",
+    databaseURL: "https://quidoo-87f0a.firebaseio.com",
+    storageBucket: "quidoo-87f0a.appspot.com",
+    messagingSenderId: "386396484728"
+ };
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -15,7 +28,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
